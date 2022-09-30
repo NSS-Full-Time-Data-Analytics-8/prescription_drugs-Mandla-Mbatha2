9eac257e38c22a2fee510fb8a6a66f55893eeda9
Mandla Mbatha <mandlaboy@gmail.com>
2022-09-30 13:17:29 -0500

    MBATHA PRESCRiPTION final push

--- prescription Mandla.sql
+++ prescription Mandla.sql
@@ -8,11 +8,28 @@ FROM prescription
 --nppes_provider_first_name, nppes_provider_last_org_name,  
 --specialty_description, and the total number of claims.
 
-SELECT DISTINCT(total_claim_count),npi,drug_name
+SELECT MAX(total_claim_count),npi,drug_name
 FROM prescription
 GROUP BY (total_claim_count),npi,drug_name
-ORDER BY drug_name
+ORDER BY drug_name DESC;  
 
-SELECT winter_games.athlete_id, winter_games.event, summer_games.event
-FROM winter_games LEFT JOIN summer_games
-USING(athlete_id);
+SELECT prescriber.nppes_provider_first_name, prescriber.nppes_provider_last_org_name,prescriber.specialty_description,prescription.total_claim_count
+FROM prescriber LEFT JOIN prescription
+USING(npi) 
+
+_-2. a. Which specialty had the most total number of claims (totaled over all drugs)?
+
+SELECT specialty_description,total_claim_count
+FROM prescriber
+
+
+--b. Which specialty had the most total number of claims for opioids?
+
+--c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+--d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
+
+
+
+
+
+
